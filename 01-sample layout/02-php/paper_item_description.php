c1ce70a2ddba32a18d84ec573c902e76be700cb2
cmscompact <suwat@cmscompact.com>
2015-10-10 08:14:39 +0700

    barcode update
    update paper_item_description.php
    and jquery barcode

--- 01-sample layout/02-php/paper_item_description.php
+++ 01-sample layout/02-php/paper_item_description.php
@@ -5,14 +5,119 @@
  * Date: 5/9/2558
  * Time: 21:15 น.
  */
-?>
-<?php
+?><?php
 /**
  * Created by PhpStorm.
  * User: Manesz
  * Date: 30/8/2558
  * Time: 13:01 น.
  */
+?><?php
+
+include("check-permission.php");
+
+$content='';
+$id=$_GET['id'];
+$get_result=$_GET['result'];
+
+$SQL="SELECT * FROM "._TB_ITEM." WHERE id='$id' LIMIT 1; ";
+$re=mysql_query($SQL);
+$num=mysql_num_rows($re);
+
+if ($num>0) {
+	$rs=mysql_fetch_array($re);
+	
+		$item_code_prefix=$rs['item_code_prefix'];		
+		$item_code=$rs['item_code'];
+		$item_code_postfix=$rs['item_code_postfix'];
+		
+		$item_code_string=$item_code_prefix.' - '.$rs['item_code'].'/'.$item_code_postfix;
+		
+		
+		
+		$equipment_name=stripslashes($rs['equipment_name']);		
+		$department_id=$rs['department_id'];	
+		$model=stripslashes($rs['model']);	
+		$resolution=stripslashes($rs['resolution']);	
+		$calibration_range=stripslashes($rs['calibration_range']);	
+		$serial_no=stripslashes($rs['serial_no']);	
+		$id_no=$rs['id_no'];
+		
+		$attb1_1=($rs['attb1_1'] =='1' ? '<li class="child">1.1 สายไฟ Probe/Sensor, Data link</li>' : '');
+		$attb1_2=($rs['attb1_2']=='1' ? '<li class="child">1.2 สาย Adapter, หม้อแปลงไฟฟ้า</li>' : '');
+		$attb1_3=($rs['attb1_3']=='1' ? '<li class="child">1.3 ขั้วต่อเครื่องมือ</li>' : '');
+		$attb1_4=($rs['attb1_4']=='1' ? '<li class="child">1.4 คู่มือการใช้งาน</li>' : '');
+		$attb1_5=($rs['attb1_5']=='1' ? '<li class="child">1.5 Battery Charger</li>' : '');
+		
+		if ($rs['attb1_6']=='1' && $attb1_6_other!="") {
+			$attb1_6_other=stripslashes($rs['attb1_6_other']);
+		} else {
+			$attb1_6_other='';
+		}
+		
+		if ($attb1_1=="" && $attb1_2=="" && $attb1_3=="" && $attb1_4=="" && $attb1_5=="" &&  $attb1_6_other=="") {
+			$no_acc1="(ไม่มี)";
+		} else { $no_acc1=""; }
+		
+		$attb2_1=($rs['attb2_1']=='1' ? '<li class="child">2.1 กล่องเครื่องมือ/ซองใส่เครื่อง</li>' : '');
+		$attb2_2=($rs['attb2_2']=='1' ? '<li class="child">2.2 หุ้มด้วยพลาสติกกันกระแทกเครื่องมือ</li>' : '');
+		$attb2_3=($rs['attb2_3']=='1' ? '<li class="child">2.3 กล่องกระดาษเครื่องมือ</li>' : '');
+		
+		if ($rs['attb2_4']=='1' && $attb2_4_other!="") {
+			$attb2_4_other=stripslashes($rs['attb2_4_other']);
+		} else {
+			$attb2_4_other='';	
+		}
+		
+		if ($attb2_1=="" && $attb2_2=="" && $attb2_3=="" &&  $attb2_4_other=="") {
+			$no_acc2="(ไม่มี)";
+		} else { $no_acc2=""; }
+		
+		
+		if ($attb1_1=="" && $attb1_2=="" && $attb1_3=="" && $attb1_4=="" && $attb1_5=="" && $attb1_6=="" &&  $attb1_6_other=="") {
+			$no_acc1="(ไม่มี)";
+		} else { $no_acc1=""; }
+		
+		
+		$calibrate_result=stripslashes($rs['calibrate_result']);	
+		
+		$calibrate_result_D='<i class="fa fa-circle-o"></i>';
+		$calibrate_result_R='<i class="fa fa-circle-o"></i>';
+		$calibrate_result_B='<i class="fa fa-circle-o"></i>';
+		
+		if ($calibrate_result=="D") { $calibrate_result_D='<i class="fa fa-circle"></i>'; }
+		if ($calibrate_result=="R") { $calibrate_result_R='<i class="fa fa-circle"></i>'; }
+		if ($calibrate_result=="B") { $calibrate_result_B='<i class="fa fa-circle"></i>'; }
+		
+		
+		
+		
+		$iso017025=($rs['iso017025']=='1' ? ' checked ' : '');		
+		
+		$manufacturer=stripslashes($rs['manufacturer']);	
+		$cert_no=stripslashes($rs['cert_no']);	
+		$cer_pdf=$rs['cer_pdf'];	
+		
+		$inv_no=$rs['inv_no'];	
+		$invoice=($inv_no!="" ? $inv_no : ' - ');
+		
+		$quotation='-';
+		$status='-';
+		
+		
+		$note=stripslashes($rs['note']);	
+		$qty=$rs['qty'];	
+		
+		$create_dttm=$rs['create_dttm'];	
+		$receive_dttm=$rs['receive_dttm'];	
+		$update_dttm=$rs['update_dttm'];	
+		
+		$customer_id=$rs['customer_id'];	
+		
+	
+}
+
+$db->close(); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,7 +157,9 @@
         <td class="text-center"><h4>Thai Heart Calibration Co.,Ltd.</h4></td>
     </tr><!-- END : THC company name -->
     <tr><!-- item barcode image -->
-        <td class="text-center"><img src="libs/img/sample-barcode.png" style="width: 230px; height: auto; padding-top: 0px;"/></td>
+        <td class="text-center"><!-- img src="libs/img/sample-barcode.png" style="width: 230px; height: auto; padding-top: 0px;"/ -->
+        		<div id="bcTarget"></div>   
+        </td>
     </tr><!-- END : item barcode image -->
     <tr><!-- item content -->
         <td>
@@ -63,37 +170,37 @@
                 <tr><!-- Receive date -->
                     <td style="width: 100px;">Receive date</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">23/1/2558</td>
+                    <td style="width: 140px;"><?php echo DateNumberEN2TH($receive_dttm); ?></td>
                 </tr><!-- END: Receive date -->
                 <tr><!-- Equipment name -->
                     <td style="width: 100px;">Equipment name</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">Weight</td>
+                    <td style="width: 140px;"><?php echo $equipment_name; ?></td>
                 </tr><!-- END : Equipment name -->
                 <tr><!-- Manufacturer -->
                     <td style="width: 100px;">Manufacturer</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">PD</td>
+                    <td style="width: 140px;"><?php echo $manufacturer; ?></td>
                 </tr><!-- END : Manufacturer -->
                 <tr><!-- Model -->
                     <td style="width: 100px;">Model</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">-</td>
+                    <td style="width: 140px;"><?php echo ($model!="" ? $model : '-'); ?></td>
                 </tr><!-- END : Model -->
                 <tr><!-- Serial No. -->
                     <td style="width: 100px;">Serial No.</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">N/A</td>
+                    <td style="width: 140px;"><?php echo ($serial_no!="" ? $serial_no : 'N/A'); ?></td>
                 </tr><!-- END : Serial No. -->
                 <tr><!-- ID No. -->
                     <td style="width: 100px;">ID No.</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">W-15</td>
+                    <td style="width: 140px;"><?php echo ($id_no!="" ? $id_no : '-'); ?></td>
                 </tr><!-- END : ID No. -->
                 <tr><!-- Calibration Points -->
                     <td style="width: 100px;">Calibration Points</td>
                     <td style="width: 10px;">:</td>
-                    <td style="width: 140px;">5, 10, 20, 20 kg.</td>
+                    <td style="width: 140px;"><?php echo ($calibration_range!="" ? $calibration_range : '-'); ?></td>
                 </tr><!-- END : Calibration Points -->
                 <tr><!-- Lot No. -->
                     <td style="width: 100px;">Lot No.</td>
@@ -106,40 +213,42 @@
                 <tr><!-- Accessories list -->
                     <td colspan="3">
                         <ul class="list-style-none">
-                            <li>1. อุปกรณ์เสริมของเครื่อง</li>
-                            <li class="child">1.1 สายไฟ Probe/Sensor, Data link</li>
-                            <li class="child">1.2 สาย Adapter, หม้อแปลงไฟฟ้า</li>
-                            <li class="child">1.3 ขั้วต่อเครื่องมือ</li>
-                            <li class="child">1.4 คู่มือการใช้งาน</li>
-                            <li class="child">1.5 Battery Charger</li>
-                            <li class="child">
-                                <span style="width: 40px; border-bottom: none; float: left;">1.6 อื่น</span>
-                                <span class="seperator" style="width: 150px; float: left; text-align: left;">&nbsp;</span>
-                            </li>
-                            <li style="clear: both;">2. การบรรจุหีบห่อเครื่องมือจากลูกค้า</li>
-                            <li class="child">2.1 กล่องเครื่องมือ/ซองใส่เครื่อง</li>
-                            <li class="child">2.2 หุ้มด้วยพลาสติกกันกระแทกเครื่องมือ</li>
-                            <li class="child">2.3 กล่องกระดาษเครื่องมือ</li>
-                            <li class="child">
-                                <span style="width: 40px; border-bottom: none; float: left;">2.4 อื่น</span>
-                                <span class="seperator" style="width: 150px; float: left; text-align: left;">&nbsp;</span>
-                            </li>
+                        
+                            <li>1. อุปกรณ์เสริมของเครื่อง <?php echo $no_acc1; ?></li>
+                                                        
+                            <?php echo $attb1_1; ?>
+                            <?php echo $attb1_2; ?>
+                            <?php echo $attb1_3; ?>
+                            <?php echo $attb1_4; ?>
+                            <?php echo $attb1_5; ?>
+                            <?php echo $attb1_6_other; ?>
+                            
+                            <!--
+                            <li class="child">	                           
+                                <span style="width: 60px; border-bottom: none; float: left;"> <?php echo $attb1_6; ?> 1.6 อื่นๆ</span>
+                                <span class="seperator" style="width: 150px; float: left; text-align: left;"><?php echo $attb1_6_other; ?></span>
+                            </li>-->
+                            <li style="clear: both;">2. การบรรจุหีบห่อเครื่องมือจากลูกค้า <?php echo $no_acc2; ?></li>
+                            <?php echo $attb2_1; ?>
+                            <?php echo $attb2_2; ?>
+                            <?php echo $attb2_3; ?>
+                            <?php echo $attb2_4_other; ?>
                         </ul>
                     </td>
                 </tr><!-- End : Accessories list -->
                 <tr><!-- Note -->
                     <td colspan="3" style="">
                         <b>Note:</b>
-                        <p style="min-height: 100px;"></p>
+                        <p style="min-height: 100px;"><?php echo ($note!="" ? nl2br($note) : '-'); ?></p>
                     </td>
                 </tr><!-- END : Note -->
                 <tr><!-- item status -->
                     <td colspan="3">
                         <table style="width: 100%;">
                             <tr>
-                                <td style="width: 83px;"><i class="fa fa-circle-o"></i> done</td>
-                                <td style="width: 83px;"><i class="fa fa-circle-o"></i> repainng</td>
-                                <td style="width: 83px;"><i class="fa fa-circle-o"></i> broken</td>
+                                <td style="width: 83px;"><?php echo $calibrate_result_D; ?> done</td>
+                                <td style="width: 83px;"><?php echo $calibrate_result_R; ?> repainng</td>
+                                <td style="width: 83px;"><?php echo $calibrate_result_B; ?> broken</td>
                             </tr>
                         </table>
                     </td>
@@ -151,5 +260,31 @@
         </td>
     </tr><!-- END : item content -->
 </table><!-- END : main table container -->
+
+<script src="libs/js/jquery-1.8.3.min.js"></script>
+<script src="libs/js/jquery-barcode-2.0.3/jquery-barcode.min.js"></script>
+
+<script>
+$(function() {
+	
+	//ดูตัวอย่างที่ http://barcode-coder.com/en/barcode-jquery-plugin-201.html
+	/*
+	type (string)
+		codabar
+		code11 (code 11)
+		code39 (code 39)
+		code93 (code 93)
+		code128 (code 128)
+		ean8 (ean 8)
+		ean13 (ean 13)
+		std25 (standard 2 of 5 - industrial 2 of 5)
+		int25 (interleaved 2 of 5)
+		msi
+		datamatrix (ASCII + extended)
+	*/
+	$("#bcTarget").barcode("<?php echo "$item_code/$item_code_postfix"; ?>", "codabar",{barWidth:2, barHeight:40, fontSize:14});     
+	//{barWidth:2, barHeight:30}
+});
+</script>
 </body>
 </html>
